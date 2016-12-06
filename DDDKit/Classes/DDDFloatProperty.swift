153173fe3428e2a69ed0547e893337d51eec6a01
Guillaume Sabran <sabranguillaume@gmail.com>
2016-12-06 12:27:26 -0800

    add float property

---
+++ DDDKit/Classes/DDDFloatProperty.swift
@@ -0,0 +1,26 @@
+//
+//  DDDFloatProperty.swift
+//  Pods
+//
+//  Created by Guillaume Sabran on 12/6/16.
+//
+//
+
+import Foundation
+import GLKit
+
+/// a shader property that contains a float value
+public class DDDFloatProperty: DDDProperty {
+	private let value: Float
+	/**
+	Create the property
+
+	- Parameter value: the float value
+	*/
+	public init(_ value: CGFloat) {
+		self.value = Float(value)
+	}
+	override func attach(at location: GLint) {
+		glUniform1f(location, value)
+	}
+}
